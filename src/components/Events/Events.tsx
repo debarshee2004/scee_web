2d88ad6198fb2240e308d0b5881607ff07b6d752
debarshee2004 <debarsheechakraborty.11d@gmail.com>
2024-03-09 23:23:41 +0530

    Adding the events section

---
+++ src/components/Events/Events.tsx
@@ -0,0 +1,31 @@
+import './Events.css'
+
+const Events = () => {
+  return (
+    <>
+        <div className="event-body">
+            <div className='event-details'>
+                <h1 className='event-title'>
+                    IEI Students' Chapter of Electrical Engineering.
+                </h1>
+                <p className='event-info'>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae expedita vel tenetur excepturi velit pariatur dolor
+                </p>
+                <div className='event-actions'>
+                    <button className='event-collaborate'>
+                        Collaborate
+                    </button>
+                    <button className='event-joinus'>
+                        Join Us
+                    </button>
+                </div>
+            </div>
+            <div className='event-hero'>
+
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default Events
