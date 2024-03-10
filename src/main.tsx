d2b73138fd7b8654e5c4bbc80be49d95d126dd18
debarshee2004 <debarsheechakraborty.11d@gmail.com>
2024-03-11 01:14:28 +0530

    On scroll animation added

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import App from './App.tsx'
-import Navbar from './components/Navbar/Navbar.tsx'
+// import Navbar from './components/Navbar/Navbar.tsx'
 import './style/main.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Navbar/>
+    {/* <Navbar/> */}
     <App />
   </React.StrictMode>,
 )
