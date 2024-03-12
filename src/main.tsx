aa5f9950df7f1fd37d6535039f5a8470f7522480
debarshee2004 <debarsheechakraborty.11d@gmail.com>
2024-03-12 08:14:07 +0530

    Added Custom Cursor

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import App from './App.tsx'
-// import Navbar from './components/Navbar/Navbar.tsx'
+import Cursor from './components/Cursor/Cursor.tsx'
 import './style/main.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    {/* <Navbar/> */}
+    <Cursor/>
     <App />
   </React.StrictMode>,
 )
