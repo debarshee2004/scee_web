96d776243bd33bd2913b521beb38e8e4b0529a28
debarshee2004 <debarsheechakraborty.11d@gmail.com>
2024-03-12 21:23:03 +0530

    Started working on the navbar

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './style/main.css'
 import Cursor from './components/Cursor/Cursor.tsx'
+import Navbar from './components/Navbar/Navbar.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Cursor/>
+    <Navbar/>
     <App />
   </React.StrictMode>,
 )
