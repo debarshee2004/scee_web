05691b8ee588c4cd3c6d8d74f35ee5cb45e5dfad
debarshee2004 <debarsheechakraborty.11d@gmail.com>
2024-03-13 22:53:54 +0530

    Completed the Routing

---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,66 @@
+import { faBolt, faCircleArrowRight } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import "./Home.css";
+import Reveal from "../Reveal/Reveal";
+
+const Home = () => {
+  return (
+    <>
+      <div className="event-body">
+        <div className="event-details">
+          <Reveal>
+            <p className="event-header">
+              <FontAwesomeIcon className="header-star" icon={faBolt} />
+              <div className="event-header-title">
+                <span className="isc">IEI STUDENT'S CHAPTER</span>{" "}
+                <span>OF ELECTRICAL ENGINEERING</span>
+              </div>
+            </p>
+          </Reveal>
+          <Reveal>
+            <h1 className="event-title">
+              ELECTRO FORGE
+              <span>
+                2024
+                <span className="dot">.</span>
+              </span>
+            </h1>
+          </Reveal>
+          <Reveal>
+            <p className="event-info">
+              A yearly event organized by IEI SCEE AOT about PCB, IOT and
+              circuit designs. This focuses on building the basic knowledge of
+              an Electrical Engineer so that they can create their own circuits.
+            </p>
+          </Reveal>
+          <div className="event-actions">
+            <a href="">
+              <button className="event-collaborate">
+                Collaborate
+                <FontAwesomeIcon
+                  className="button-arrow"
+                  icon={faCircleArrowRight}
+                />
+              </button>
+            </a>
+            <a href="">
+              <button className="event-joinus">
+                Participate
+                <FontAwesomeIcon
+                  className="button-arrow-two"
+                  icon={faCircleArrowRight}
+                />
+              </button>
+            </a>
+          </div>
+        </div>
+        <div className="event-hero">
+          
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,40 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-import App from './App.tsx'
-import './style/main.css'
-import Cursor from './components/Cursor/Cursor.tsx'
-import Navbar from './components/Navbar/Navbar.tsx'
+import "./main.css";
+import Cursor from "./components/Cursor/Cursor.tsx";
+import Navbar from "./components/Navbar/Navbar.tsx";
+import Home from "./components/Home/Home.tsx";
+import Team from "./components/Teams/Team.tsx";
+import Faculty from "./components/Faculty/Faculty.tsx";
+import Events from "./components/Events/Events.tsx";
+// import Error from "./components/Error/Error.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    // errorElement: <Error />
+  },
+  {
+    path: "/team",
+    element: <Team />
+  },
+  {
+    path: "/faculty",
+    element: <Faculty />
+  },
+  {
+    path: "/events",
+    element: <Events />
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Cursor/>
-    <Navbar/>
-    <App />
-  </React.StrictMode>,
-)
+    <Cursor />
+    <Navbar />
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
