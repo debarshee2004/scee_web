7e9757febebffaa92c6beb3fe20f773fa40ccd23
debarshee2004 <debarsheechakraborty.11d@gmail.com>
2024-03-14 21:00:07 +0530

    Added react router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,23 @@
+import { Route,Routes } from "react-router-dom"
+
+import "./App.css"
+import Home from "./pages/Home/Home"
+import Team from "./pages/Team/Team"
+import Faculty from "./pages/Faculty/Faculty"
+import Events from "./pages/Events/Events"
+import Error from "./pages/Error/Error"
+
 const App = () => {
   return (
-    <div>App</div>
+    <main>
+      <Routes>
+        <Route index element={<Home/>} />
+        <Route path="/team" element={<Team/>} />
+        <Route path="/faculty" element={<Faculty/>} />
+        <Route path="/events" element={<Events/>} />
+        <Route path="*" element={<Error/>} />
+      </Routes>
+    </main>
   )
 }
 
