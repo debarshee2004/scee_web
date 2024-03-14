7f562117d9cf44f357a470c730465ab7952d2ad9
debarshee2004 <debarsheechakraborty.11d@gmail.com>
2024-03-14 21:04:22 +0530

    Testing React router

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,6 +1,13 @@
+import { Link } from "react-router-dom"
+import "./Home.css"
+
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+        <Link to="/team">
+        Home
+        </Link>
+    </div>
   )
 }
 
