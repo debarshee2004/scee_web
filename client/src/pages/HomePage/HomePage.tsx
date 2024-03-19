f216c4ae9fdbcbe9784fdc4cb4dc6148a6e4f091
debarshee2004 <debarsheechakraborty.11d@gmail.com>
2024-03-19 22:30:56 +0530

    Added the Homescreen

---
+++ client/src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,14 @@
+import Events from "../../components/Events/Events"
+import Navbar from "../../components/Navbar/Navbar"
+import "./HomePage.css"
+
+const HomePage = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Events/>
+    </div>
+  )
+}
+
+export default HomePage
